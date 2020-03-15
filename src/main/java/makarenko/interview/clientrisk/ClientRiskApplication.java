d1fb4eff484b9b86dc6dd24ab250f1690f50ee0a
Alexey Makarenko <alexey-makarenko@hotmail.com>
2020-03-15 19:48:35 +0300

    Initial project

---
+++ src/main/java/makarenko/interview/clientrisk/ClientRiskApplication.java
@@ -0,0 +1,13 @@
+package makarenko.interview.clientrisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientRiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientRiskApplication.class, args);
+    }
+
+}
